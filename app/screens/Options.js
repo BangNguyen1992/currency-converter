94d8bfba644e20d207c01dd5374df95ec5659dbe
Bang Nguyen <nguyenbahaibang@gmail.com>
2018-10-21 17:35:24 +0300

    remove comment code

--- app/screens/Options.js
+++ app/screens/Options.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-// import PropTypes from 'prop-types';
 import { ScrollView, StatusBar, Platform } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
 
