1669e1f4b3d74450445f3314d65271b799bd2c93
Bang Nguyen <nguyenbahaibang@gmail.com>
2018-10-21 17:35:40 +0300

    add Themes screen

---
+++ app/screens/Themes.js
@@ -0,0 +1,57 @@
+import React, { Component } from 'react';
+import { ScrollView, StatusBar, Platform } from 'react-native';
+import EStyleSheet from 'react-native-extended-stylesheet';
+
+import { ListItem, Separator } from '../components/List';
+
+const styles = EStyleSheet.create({
+  $blue: '$primaryBlue',
+  $green: '$primaryGreen',
+  $orange: '$primaryOrange',
+  $purple: '$primaryPurple',
+});
+
+// export default styles;
+
+export default class Themes extends Component {
+  handleThemePress = (color) => {
+    console.log('object change theme color to ', color);
+  }
+
+  render() {
+    return (
+      <ScrollView>
+        <StatusBar translucent={false} barStyle="default" />
+        <ListItem
+          text="Blue"
+          onPress={() => this.handleThemePress(styles.$blue)}
+          selected
+          checkmark={false}
+          iconBackground={styles.$blue}
+        />
+        <ListItem
+          text="Green"
+          onPress={() => this.handleThemePress(styles.$green)}
+          selected
+          checkmark={false}
+          iconBackground={styles.$green}
+        />
+        <ListItem
+          text="Orange"
+          onPress={() => this.handleThemePress(styles.$orange)}
+          selected
+          checkmark={false}
+          iconBackground={styles.$orange}
+        />
+        <ListItem
+          text="Purple"
+          onPress={() => this.handleThemePress(styles.$purple)}
+          selected
+          checkmark={false}
+          iconBackground={styles.$purple}
+        />
+      </ScrollView>
+
+    )
+  }
+}
