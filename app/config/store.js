a6566a0eadd5c2207a81eb6ca79fea15477d6f7c
Bang Nguyen <nguyenbahaibang@gmail.com>
2018-10-22 12:53:19 +0300

    add saga middleware to redux store

--- app/config/store.js
+++ app/config/store.js
@@ -1,11 +1,19 @@
 import { createStore, applyMiddleware } from 'redux';
+import createSagaMiddleware from 'redux-saga';
 import logger from 'redux-logger';
 
 import reducers from '../reducers';
+import rootSaga from './sagas';
 
-const middleWare = [];
-// if (process.env.NODE_ENV === 'development') {
-//   middleWare.push(logger);
-// }
+const sagaMiddleware = createSagaMiddleware();
+const middleware = [sagaMiddleware];
 
-export default createStore(reducers, applyMiddleware(...middleWare));
+if (process.env.NODE_ENV === 'development') {
+  middleware.push(logger);
+}
+
+const store = createStore(reducers, applyMiddleware(...middleware));
+
+sagaMiddleware.run(rootSaga);
+
+export default store;
